44cc87bdbe4e72e50ba624d99129f8ceffedea7e
DBM <diegobarmor42@gmail.com>
2025-12-06 16:29:13 +0100

    (adventofcode) added 2015-05, 2025-06

--- adventofcode/2015/day_05/main.py
+++ adventofcode/2015/day_05/main.py
@@ -1,25 +1,49 @@
 VOWELS = set("aeiou")
 FORBIDDEN = {('a','b'), ('c','d'), ('p','q'), ('x','y')}
 
-def at_least_3_vowels(chars: set[str]):
-    # return len(chars.intersection(VOWELS)) >= 3
+def get_pairs(chars: str):
+    return (
+        set(zip(chars[ :-1:2], chars[1::2])),
+        set(zip(chars[1:-1:2], chars[2::2]))
+    )
+
+def at_least_3_vowels(chars: str):
     return sum(c in VOWELS for c in chars) >= 3
 
-def has_double(pairs: set[tuple[str, str]]):
+def has_repeated(pairs: set[tuple[str, str]]):
     return any(map(lambda t: t[0] == t[1], pairs))
 
 def not_forbidden(pairs: set[tuple[str, str]]):
     return not pairs.intersection(FORBIDDEN)
 
-def is_nice(s: str) -> bool:
-    if len(s) < 3: return False
+def has_double_pairs(chars: str):
+    for i in range(len(chars)-2):
+        pair_i = (chars[i], chars[i+1])
+        for j in range(i+2, len(chars)-1):
+            pair_j = (chars[j], chars[j+1])
+            if pair_i == pair_j:
+                return True
+    return False
 
-    pairs = set(zip(s[:-1:2], s[1::2])) | set(zip(s[1:-1:2], s[2::2]))
-    return at_least_3_vowels(s) and has_double(pairs) and not_forbidden(pairs)
+def has_repeated_with_inbetween(chars: str):
+    p0_a,p1_a = get_pairs(chars[ ::2])
+    p0_b,p1_b = get_pairs(chars[1::2])
+    return has_repeated(p0_a|p1_a) or has_repeated(p0_b|p1_b)
+
+
+def is_nice_v1(s: str) -> bool:
+    if len(s) < 3: return False
+    p0,p1 = get_pairs(s)
+    return at_least_3_vowels(s) and has_repeated(p0|p1) and not_forbidden(p0|p1)
+
+def is_nice_v2(s: str) -> bool:
+    if len(s) < 4: return False
+    return has_repeated_with_inbetween(s) and (has_double_pairs(s))
+
 
 with open("input.txt", 'r') as file:
     data = file.read().strip().split()
 
 
-print(sum(map(is_nice, data))) # PART 1: 258
-print() # PART 2:
+print(sum(map(is_nice_v1, data))) # PART 1: 258
+print(sum(map(is_nice_v2, data))) # PART 2: 53
---
+++ adventofcode/2025/day_06/main.py
@@ -0,0 +1,37 @@
+import math
+
+with open("input.txt", 'r') as file:
+    data = file.read().strip('\n').split('\n')
+
+def map_op(c: str) -> "callable | None":
+    if c == '+': return sum
+    if c == '*': return math.prod
+
+def iter_v0(data):
+    lst_nums = zip(*(row.strip().split() for row in data[:-1]))
+    lst_ops  = map(map_op, data[-1].strip().split())
+    return zip(lst_nums, lst_ops)
+
+def iter_v1(data):
+    nums = []
+    for column in reversed(tuple(zip(*data))):
+        # n = ''.join(c for c in column[:-1] if c != ' ') ### safer...
+        n = ''.join(column[:-1]).strip()
+        if not n:
+            nums = []; continue
+
+        nums.append(n)
+        op = map_op(column[-1])
+        if op is None: continue
+        yield nums, op
+
+def get_total(obj_iter):
+    return sum(
+        op(int(n) for n in nums) for nums,op in obj_iter
+    )
+
+lst_nums = zip(*(row.strip().split() for row in data[:-1]))
+lst_ops  = map(map_op, data[-1].strip().split())
+
+print(get_total(iter_v0(data))) # PART 1: 7098065460541
+print(get_total(iter_v1(data))) # PART 2: 13807151830618
